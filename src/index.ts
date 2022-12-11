37e6c47fff96754685ce4f2635dfe5d26a8a4c5d
AsPulse <aspulse.x@gmail.com>
2022-12-11 23:34:09 +0900

    Add: logger

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,13 @@ import type { ApiRequestBase, ApiResponseBase, ApiSchemaBase, HttpRequestMethod}
 import { HTTP_REQUEST_METHODS } from '@nutsapi/types';
 import type { IncomingMessage, Server, ServerResponse } from 'http';
 import { createServer } from 'http';
+import type { UrlWithParsedQuery } from 'url';
 import { parse } from 'url';
 import type { Handler, AllEndPoint, AllMethod, WorkerType} from './handler';
 import { NutsAPIHandler } from './handler';
+import type { NutsLogger } from './logger';
+import { NormalNutsLogger } from './logger';
+import { getRemoteAddress } from './remoteAddress';
 import { NutsRequest } from './worker';
 
 interface CorsOption {
@@ -16,7 +20,7 @@ interface CorsOption {
 
 export class NutsAPIServer<Schema extends ApiSchemaBase> {
   private endpoints: { endpoint: string, method: string, type: { request: ApiRequestBase, response: ApiResponseBase } }[];
-  constructor(public schema: Schema) {
+  constructor(public schema: Schema, public logger: NutsLogger = new NormalNutsLogger()) {
     this.endpoints = Object.entries(schema).flatMap(v => Object.entries(v[1]).map(e => ({ endpoint: v[0], method: e[0], type: e[1] })));
   }
 
@@ -56,40 +60,17 @@ export class NutsAPIServer<Schema extends ApiSchemaBase> {
     this.corsOptions.push(option);
   }
 
-  private async handleRequest(req: IncomingMessage, res: ServerResponse<IncomingMessage>) {
+  private async handleRequest(req: IncomingMessage, url: UrlWithParsedQuery | undefined): Promise<{ headers: Record<string, string | string[]>, code: number, payload: string | null }> {
 
-    if(req.method === 'OPTIONS') {
-      const origin = req.headers.origin;
-      if(origin === undefined) {
-        res.writeHead(204);
-        res.end();
-        return;
-      }
-      const option = this.corsOptions.find(v => v.origin === origin);
-      if(option !== undefined) {
-        res.writeHead(204, {
-          'Access-Control-Allow-Origin': origin,
-          'Access-Control-Allow-Headers': option.headers.join(', '),
-          'Access-Control-Allow-Methods': option.methods.join(', '),
-          ...(option.credential ? { 'Access-Control-Allow-Credentials': 'true' } : {}),
-        });
-      } else {
-        res.writeHead(204);
-      }
-      res.end();
-      return;
-    }
+    if(url === undefined) return this.responseError(500);
 
-    if(req.url === undefined) return this.responseError(res, 500);
-
-    const url = parse(req.url, true);
     if(req.method === 'GET' || (req.headers['content-type'] !== 'application/json' && req.method === 'DELETE')) {
-      return this.handleEndPoint(req.method, url.pathname ?? '/', url.query, req, res);
+      return this.handleEndPoint(req.method, url.pathname ?? '/', url.query, req);
     }
 
-    if(req.headers['content-type'] !== 'application/json') return this.responseError(res, 400);
+    if(req.headers['content-type'] !== 'application/json') return this.responseError(400);
 
-    try {
+    return new Promise(resolve => {
       const body: Uint8Array[] = [];
       req.on('error', (err) => {
         console.error(err);
@@ -97,66 +78,111 @@ export class NutsAPIServer<Schema extends ApiSchemaBase> {
         body.push(chunk);
       }).on('end', () => {
         const receivedBody = Buffer.concat(body).toString();
-        const payload = JSON.parse(receivedBody);
-        return this.handleEndPoint(req.method ?? '', url.pathname ?? '/', payload, req, res);
+        try {
+          const payload = JSON.parse(receivedBody);
+          resolve(this.handleEndPoint(req.method ?? '', url.pathname ?? '/', payload, req));
+          return;
+        } catch {
+          resolve(this.responseError(400));
+          return;
+        }
       });
-    } catch {
-      return this.responseError(res, 400);
-    }
+    });
   }
 
-  private async handleEndPoint(method: string, endpoint: string, payload: unknown, req: IncomingMessage, res: ServerResponse<IncomingMessage>) {
+  private async handleEndPoint(method: string, endpoint: string, payload: unknown, req: IncomingMessage) {
     const handler = this.listeningHanders.find(v => v.endpoint === endpoint && v.method === method);
     const schema = this.endpoints.find(v => v.endpoint === endpoint && v.method === method);
 
     if(handler === undefined || schema === undefined) {  
-      return this.responseError(res, schema !== undefined ? 501 : 404);
+      return this.responseError(schema !== undefined ? 501 : 404);
     }
 
     const parsedPayload = schema.type.request.safeParse(payload);
-    if(!parsedPayload.success) return this.responseError(res, 400);
+    if(!parsedPayload.success) return this.responseError(400);
 
     
     const nutsRequest = new NutsRequest<unknown, never>(parsedPayload.data, req);
     await handler.worker(nutsRequest);
 
     const response = NutsRequest.UNPACK(nutsRequest);
-    if(response === null) return this.responseError(res, 500);
+    if(response === null) return this.responseError(500);
     try{
       const content = JSON.stringify(response.payload);
-      res.writeHead(response.code, {
-        'Content-Type': 'application/json',
-        ...(response.cookie === null ? {} : { 'Set-Cookie': response.cookie }),
-      });
-      res.end(content, 'utf-8');
-      return;
+      return {
+        code: response.code,
+        headers: {
+          'Content-Type': 'application/json',
+          ...(response.cookie === null ? {} : { 'Set-Cookie': response.cookie }),
+        },
+        payload: content,
+      };
     } catch {
-      return this.responseError(res, 500);
+      return this.responseError(500);
     }
   }
 
-  private async responseError(res: ServerResponse<IncomingMessage>, code: 400 | 404 | 500 | 501) {
-    res.writeHead(code);
+  private async responseError(code: 400 | 404 | 500 | 501) {
     switch(code) {
     case 400:
-      res.end(this.messages.badRequest, 'utf-8');
-      break;
+      return { code, payload: this.messages.badRequest, headers: {} };
     case 404:
-      res.end(this.messages.notFound, 'utf-8');
-      break;
+      return { code, payload: this.messages.notFound, headers: {} };
     case 500:
-      res.end(this.messages.internalServerError, 'utf-8');
-      break;
+      return { code, payload: this.messages.internalServerError, headers: {} };
     case 501:
-      res.end(this.messages.notImplemented, 'utf-8');
-      break;
+      return { code, payload: this.messages.notImplemented, headers: {} };
     }
+  }
+
+  private async listener(req: IncomingMessage, res: ServerResponse<IncomingMessage>) {
+    const origin = req.headers.origin;
+    const option = this.corsOptions.find(v => v.origin === origin);
+    const remoteAddress = getRemoteAddress(req.headers, req.socket.remoteAddress);
+
+    const cors = origin !== undefined && option !== undefined ? {
+      'Access-Control-Allow-Origin': origin,
+      'Access-Control-Allow-Headers': option.headers.join(', '),
+      'Access-Control-Allow-Methods': option.methods.join(', '),
+      ...(option.credential ? { 'Access-Control-Allow-Credentials': 'true' } : {}),
+    } : {};
+
+    const url = req.url !== undefined ? parse(req.url, true) : req.url;
+
+    if(req.method === 'OPTIONS') {
+      if(origin === undefined || option === undefined) {
+        res.writeHead(204);
+      } else {
+        res.writeHead(204, cors);
+      }
+      res.end();
+
+      this.logger.request({
+        method: 'OPTIONS',
+        path: url?.pathname ?? '',
+        statusCode: 204,
+        remoteAddress: remoteAddress ?? 'unknown',
+      });
+      return;
+    }
+
+    const result = await this.handleRequest(req, url);
+
+    res.writeHead(result.code, { ...result.headers, ...cors });
+    if(result.payload === null) { res.end(); } else { res.end(result.payload, 'utf-8'); }
+
+    this.logger.request({
+      method: req.method ?? '',
+      path: url?.pathname ?? '',
+      statusCode: result.code,
+      remoteAddress: remoteAddress ?? 'unknown',
+    });
   }
 
   public listen(port: number) {
     this.listeningHanders = [this.handlers, ...this.extHandlers.map(v => NutsAPIHandler.UNPACK(v))].flat();
     this.server = createServer((req, res) => {
-      void this.handleRequest(req, res);
+      void this.listener(req, res);
     });
     this.server.listen(port);
   }
@@ -167,4 +193,5 @@ export class NutsAPIServer<Schema extends ApiSchemaBase> {
 }
 
 export { NutsAPIHandler } from './handler';
+export { NutsLogger } from './logger';
 export type { NutsRequest } from './worker';
